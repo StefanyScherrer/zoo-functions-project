e61d1b259f7c5dcf363f5371e2632d640b56b7c2
Stefany Scherrer <devstefanyscherrer@gmail.com>
2022-05-26 18:24:07 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,25 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se averageAge retorna um nº', () => {
+    expect(typeof handlerElephants('averageAge')).toBe('number');
+  });
+  it('Verifica se param retorna um objeto', () => {
+    expect(typeof handlerElephants('param')).toBe('object');
+  });
+  it('Verifica se elephants retorna um objeto', () => {
+    expect(typeof handlerElephants('elephants')).toBe('object');
+  });
+  it('Teste se o argumento location deve retornar a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  it('Verifica se computeData retorna um objeto', () => {
+    expect(typeof handlerElephants('computeData')).toBe('object');
+  });
+  it('Verifica se popularity retorna um nº', () => {
+    expect(typeof handlerElephants('popularity')).toBe('number');
+  });
+});
