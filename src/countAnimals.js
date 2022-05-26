0c8832912b57da0e978e665e3d4d9a52717e6dbc
Stefany Scherrer <devstefanyscherrer@gmail.com>
2022-05-26 16:23:05 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const countAllAnimals = () => {
+  const { species } = data;
+  const firstCount = species.reduce((acc, { name, residents }) => {
+    acc[name] = residents.length;
+    return acc;
+  }, {});
+  return firstCount;
+};
+
+const selectedAnimalCount = (specie, gender) => {
+  const currentSpecie = data.species.find(({ name }) => name === specie);
+  const secondCount = gender
+    ? currentSpecie.residents
+      .filter(({ sex }) => sex === gender).length
+    : currentSpecie.residents.length;
+  return secondCount;
+};
+
 function countAnimals(animal) {
   // seu código aqui
+  const thirdCount = animal ? selectedAnimalCount(animal.specie, animal.sex) : countAllAnimals();
+  return thirdCount;
 }
 
 module.exports = countAnimals;
