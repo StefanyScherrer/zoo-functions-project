fe64c4b4e9a94ee800ea28a51b34cb21a50fba69
Stefany Scherrer <devstefanyscherrer@gmail.com>
2022-05-26 17:32:24 -0300

    Requisitos 10, 11, 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+// function getAnimalMap(options) {
+//   // seu código aqui
+// }
+const species = (animal) => !animal || !animal.includeNames;
+
+const sexSieve = (residents, animal) =>
+  residents.filter(({ sex: residentSex }) => animal.sex === residentSex);
+
+const getResidentNames = (animal, residents, specieName) => {
+  const filteredResidents = animal.sex ? sexSieve(residents, animal) : residents;
+  let residentNames = [...filteredResidents.map((resident) => resident.name)];
+  residentNames = animal.sorted ? residentNames.sort() : residentNames;
+  const result = {};
+  result[specieName] = residentNames;
+  return result;
+};
+
+const getAnimalByLocation = (animal) =>
+  data.species.reduce((acc, { location, name: specieName, residents }) => {
+    acc[location] = acc[location] || [];
+
+    const specieInfo = species(animal)
+      ? specieName
+      : getResidentNames(animal, residents, specieName);
+
+    acc[location].push(specieInfo);
+
+    return acc;
+  }, {});
+
+function getAnimalMap(animal) {
+  const animalMap = getAnimalByLocation(animal);
+  return animalMap;
 }
+getAnimalMap({ includeNames: true, sorted: true });
 
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = data.employees.find((e) => e.id === id);
+  const animal = data.species.find((specie) => specie.id === employee.responsibleFor[0]);
+  console.log(animal);
+  animal.residents.sort((a, b) => b.age - a.age);
+  const { name, sex, age } = animal.residents[0];
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
