a7b6c0c4e3dfb104c3916bd5f0ee6b9763761475
Stefany Scherrer <devstefanyscherrer@gmail.com>
2022-05-26 16:47:24 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,42 @@
 const data = require('../data/zoo_data');
 
+// function countEntrants(entrants) {
+//   // seu código aqui
+// }
+
+// function calculateEntry(entrants) {
+// }
+function stageOfLife(age) {
+  let stage1 = 'child';
+  if (age >= 50) stage1 = 'senior';
+  if (age >= 18 && age < 50) stage1 = 'adult';
+  return stage1;
+}
 function countEntrants(entrants) {
-  // seu código aqui
+  const accumulator = entrants.reduce((acc, { age }) => {
+    const stage2 = stageOfLife(age);
+    acc[stage2] = acc[stage2] || 0;
+    acc[stage2] += 1;
+    return acc;
+  }, {});
+  return accumulator;
+}
+
+function integralValue(total) {
+  const { prices } = data;
+  return Object.keys(total).reduce((acc, age) => {
+    const paid = total[age];
+    const price = prices[age];
+    const result = paid * price;
+    return acc + result;
+  }, 0);
 }
 
 function calculateEntry(entrants) {
+  if (!entrants || Object.entries(entrants).length === 0) return 0;
+  const allEntrants = countEntrants(entrants);
+  const result = integralValue(allEntrants);
+  return result;
 }
 
 module.exports = { calculateEntry, countEntrants };
